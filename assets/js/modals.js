f1acdca0de602a6aaa66741eef93bd4c9049af71
Stevie <stevie.nikol.bales@gmail.com>
2016-08-09 16:21:09 -0500

    The Beginning

---
+++ assets/js/modals.js
@@ -0,0 +1,39 @@
+    function overlay1() {
+        el = document.getElementById("USAC");
+        el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+    }
+
+    function overlay2() {
+        el = document.getElementById("missed-connections");
+        el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+    }
+
+    function overlay3() {
+        el = document.getElementById("proper-hijinx");
+        el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+    }
+
+    function overlay4() {
+        el = document.getElementById("edinlore");
+        el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+    }
+
+    function overlay5() {
+        el = document.getElementById("ambience");
+        el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+    }
+
+    function overlay6() {
+        el = document.getElementById("writers-museum");
+        el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+    }
+
+    function overlay7() {
+        el = document.getElementById("nae-place");
+        el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+    }
+
+    function overlay8() {
+        el = document.getElementById("endgame");
+        el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+    }
---
+++ assets/php/contact.php
@@ -0,0 +1,32 @@
+<?php
+$field_name = $_POST['cf_name'];
+$field_email = $_POST['cf_email'];
+$field_message = $_POST['cf_message'];
+
+$mail_to = 'design@steviebales';
+$subject = 'You have a new message from '.$field_name;
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Message: '.$field_message;
+
+$headers = 'From: '.$field_name."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+if ($mail_status) { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Thank you for reaching out to me. I''ll get back to you as soon as possible.');
+		window.location = 'index.html#contact';
+	</script>
+<?php
+}
+else { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Message failed. Please, send an email to design@steviebales.com');
+		window.location = 'index.html#contact';
+	</script>
+<?php
+}
+?>
